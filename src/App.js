d3ccafa3c77db482761a11585cec04d42a88804e
platon.ivankiv <ivankiv.platon@gmail.com>
2023-11-22 16:44:06 +0300

    add json

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './styles/App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Route path="/about">
+                <About />
+            </Route>
+
+            <Route path="/posts">
+                <Posts />
+            </Route>
+        </BrowserRouter>
+    )
+}
+
+export default App;
